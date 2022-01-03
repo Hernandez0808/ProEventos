3c3baa1dcd0ab206bc29e2e850848d481a018c59
Daniel <daniel0808soares@gmail.com>
2022-01-02 22:50:59 -0300

    Seção 3 EF

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,57 +13,61 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-             new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular e suas nov",
-                Local = "São Paulo",
-                QtdPessoas = 280,
-                DataEvento = DateTime.Now.AddDays(3).ToString(),
-                ImagemURL = "foto2.png"
-            }
-        
-            };
-        
-        public EventoController()
+        // public IEnumerable<Evento> _evento = new Evento[]{
+        //      new Evento(){
+        //         EventoId = 1,
+        //         Tema = "Angular 11 e .NET 5",
+        //         Local = "Belo Horizonte",
+        //         QtdPessoas = 250,
+        //         DataEvento = DateTime.Now.AddDays(2).ToString(),
+        //         ImagemURL = "foto.png"
+        //     },
+        //     new Evento(){
+        //         EventoId = 2,
+        //         Tema = "Angular e suas nov",
+        //         Local = "São Paulo",
+        //         QtdPessoas = 280,
+        //         DataEvento = DateTime.Now.AddDays(3).ToString(),
+        //         ImagemURL = "foto2.png"
+        //     }
+
+        //     };
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-      
+          _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento =>
+             evento.EventoId == id
+             );
         }
-             [HttpPost]
+
+        [HttpPost]
         public string Post()
         {
             return "Exemplo POST";
         }
 
-               [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public string Put(int id)
         {
-            return $"Exemplo Put = {id}" ;
+            return $"Exemplo Put = {id}";
         }
 
-               [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string DeHttpDelete(int id)
         {
-            return $"Exemplo DeHttpDelete = {id}" ;
+            return $"Exemplo DeHttpDelete = {id}";
         }
     }
 }
